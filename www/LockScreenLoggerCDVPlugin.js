7cfa6451d0b3b5cfe0e957f9551c9032ddd0661a
Timofey Tatarinov <tatarinov.timofey@gmail.com>
2014-09-17 15:05:47 +0400

    no message

--- www/LockScreenLoggerCDVPlugin.js
+++ www/LockScreenLoggerCDVPlugin.js
@@ -1,43 +1,32 @@
-(function(){
-    var screenOn = function(){};
-    var screenOff = function(){};
+var LockScreenLoggerCDVPlugin = {
+    screenOn: function(){},
+    screenOff: function(){},
 
-    function lockScreen(){
-    }
 
-    var p = lockScreen.prototype;
-
-    p.start = function(screenOnCallback, screenOffCallback, errorCallback){
+    start: function(screenOnCallback, screenOffCallback, errorCallback){
         screenOn = screenOnCallback;
         screenOff = screenOffCallback;
         !errorCallback && (errorCallback = function(){});
 
         cordova.exec(function (arg) {
         }, errorCallback, "LockScreenLoggerCDVPlugin", "init", []);
-    };
+    },
 
-    p.status = function(resultCallback, cancelCallback, errorCallback){
+    status: function(resultCallback, cancelCallback, errorCallback){
         cordova.exec(function (arg) {
             resultCallback(arg);
         }, errorCallback, "LockScreenLoggerCDVPlugin", "getScreenStatus", []);
-    };
+    },
 
-    p.screenon = function(data){
+    screenon: function(data){
         screenOn && screenOn(data);
-    };
+    },
 
-    p.screenoff = function(data){
+    screenoff: function(data){
         screenOff && screenOff(data);
-    };
+    }
+};
 
 
-    // define in cordova
-    cordova.define("com.citronium.lockscreenloggerplugin.LockScreenLoggerCDVPlugin", function(require, exports, module) {
 
-        var inst = new lockScreen();
-
-        window['LockScreenLoggerCDVPlugin'] = inst;
-
-        module.exports = inst;
-    });
-})();
+module.exports = LockScreenLoggerCDVPlugin;
