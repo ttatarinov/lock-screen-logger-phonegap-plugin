89aee66dec4b316c719c03dbaacc938b1b6f2dda
Timofey Tatarinov <tatarinov.timofey@gmail.com>
2014-09-17 15:16:35 +0400

    no message

--- www/LockScreenLoggerCDVPlugin.js
+++ www/LockScreenLoggerCDVPlugin.js
@@ -1,32 +1,34 @@
-var LockScreenLoggerCDVPlugin = {
-    screenOn: function(){},
-    screenOff: function(){},
-
-
-    start: function(screenOnCallback, screenOffCallback, errorCallback){
-        screenOn = screenOnCallback;
-        screenOff = screenOffCallback;
-        !errorCallback && (errorCallback = function(){});
-
-        cordova.exec(function (arg) {
-        }, errorCallback, "LockScreenLoggerCDVPlugin", "init", []);
-    },
-
-    status: function(resultCallback, cancelCallback, errorCallback){
-        cordova.exec(function (arg) {
-            resultCallback(arg);
-        }, errorCallback, "LockScreenLoggerCDVPlugin", "getScreenStatus", []);
-    },
-
-    screenon: function(data){
-        screenOn && screenOn(data);
-    },
-
-    screenoff: function(data){
-        screenOff && screenOff(data);
-    }
-};
-
-
-
-module.exports = LockScreenLoggerCDVPlugin;
+  var LockScreenLoggerCDVPlugin = {
+               m_screenOn: function () {
+               },
+               m_screenOff: function () {
+               },
+               
+               
+               start: function (screenOnCallback, screenOffCallback, errorCallback) {
+               this.m_screenOn = screenOnCallback;
+               this.m_screenOff = screenOffCallback;
+               !errorCallback && (errorCallback = function () {
+                                  });
+               
+               cordova.exec(function (arg) {
+                            }, errorCallback, "LockScreenLoggerCDVPlugin", "init", []);
+               },
+               
+               status: function (resultCallback, cancelCallback, errorCallback) {
+               cordova.exec(function (arg) {
+                            resultCallback(arg);
+                            }, errorCallback, "LockScreenLoggerCDVPlugin", "getScreenStatus", []);
+               },
+               
+               screenon: function (data) {
+               this.m_screenOn(data);
+               },
+               
+               screenoff: function (data) {
+               this.m_screenOff(data);
+               }
+               };
+               
+               
+               module.exports = LockScreenLoggerCDVPlugin;
